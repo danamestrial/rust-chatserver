9bad1a00a7ce7d6166a4a9f1026e17e995fc57d5
glorasika <rasika.ara@student.mahidol.edu>
2022-07-17 19:32:43 +0700

    login half done

--- frontend/frontend-chatserver/src/views/LoginView.vue
+++ frontend/frontend-chatserver/src/views/LoginView.vue
@@ -1,8 +1,77 @@
 <template>
-    <div id="login">
-        <h1>Login</h1>
-        <input type="text" name="username" v-model="input.username" placeholder="Username" />
-        <input type="password" name="password" v-model="input.password" placeholder="Password" />
-        <button type="button" v-on:click="login()">Login</button>
-    </div>
+   <v-app id="login">
+      <v-content>
+         <v-container fluid fill-height>
+            <v-layout align-center justify-center>
+               <v-flex xs12 sm8 md4>
+                  <v-card class="elevation-12">
+                     <v-toolbar dark color="green">
+                        <v-toolbar-title>LOGIN</v-toolbar-title>
+                     </v-toolbar>
+                     <v-card-text>
+                        <v-form>
+                            <v-col
+                                cols="12"
+                                sm="15"
+                                md="40"
+                            >
+
+                           <v-text-field
+                              name="Username"
+                              label="Username"
+                              maxlength="15"
+                              counter="15"
+                              filled
+                              rounded
+                              type="text"
+                           ></v-text-field>
+                           </v-col>
+                            <v-col
+                                cols="12"
+                                sm="15"
+                                md="40"
+                            >
+                           <v-text-field
+                              id="password"
+                              name="password"
+                              label="Password"
+                              filled
+                              rounded
+                              type="password"
+                           ></v-text-field>
+                           </v-col>
+                        </v-form>
+                     </v-card-text>
+                     <v-card-actions>
+                        <v-spacer></v-spacer>
+                            <v-col
+                                cols="12"
+                                sm="15"
+                                md="40"
+                            >
+                        <v-btn color="green"
+                            class="ma-2 white--text"
+                            rounded
+                            x-large
+                            to="/">Let's go!
+                            </v-btn>
+                            </v-col>
+                     </v-card-actions>
+                  </v-card>
+               </v-flex>
+            </v-layout>
+         </v-container>
+      </v-content>
+   </v-app>
 </template>
+
+<script>
+export default {
+   name: "LoginPage",
+   props: {
+      source: String,
+   },
+};
+</script>
+
+<style></style>
